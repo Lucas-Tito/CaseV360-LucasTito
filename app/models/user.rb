28f442968cf7ebb5e103d42ac4f3cf15d4651ccc
Lucas Tito <lucassga500@gmail.com>
2026-01-18 20:19:06 -0300

    fix: move inline css to css classes to solve csp issue
    csp: Content Security Policy

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,4 @@ class User < ApplicationRecord
   validates :name, presence: true
   validates :email, presence: true, uniqueness: true
   validates :uid, presence: true, uniqueness: true
-  validates :photo_url, presence: false # Allow null for users without photos
 end
